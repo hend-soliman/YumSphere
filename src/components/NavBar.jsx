289407bdc73b353a0fc1b46d0ed342a7f40dbe0b
hend-soliman <hend63407@gmail.com>
2026-01-10 20:32:07 +0200

    'update2'

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,59 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import { HiMenu } from "react-icons/hi";
 
 export default function NavBar() {
+
+  const links =[
+    {name: "Home", path: "/"},
+    {name: "About", path: "/about"},
+    {name: "Menu", path:"/menu"},
+    {name: "contact", path:"/contact"},
+  ];
+
+ const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="w-full bg-black">
       <div className="container mx-auto max-w-6xl">
         <div className="flex items-center justify-between py-4">
+
           <h1 className="text-3xl font-bold text-white">logo</h1>
 
-          <ul className="flex gap-4 text-white">
-            <li>
+          <ul className="hidden md:flex gap-4 text-white">
+            {links.map((link) => (
+            <li key={link.path}>
+              <NavLink to={Link.path} className={({isActive})=>`transition hover:text-[#ffc554] ${isActive ? "text-[#748194]" : "text-[#ffc554]" }`}>{link.name}</NavLink>
+            </li>
+            ))}
+          </ul>
+
+          <button className='md:hidden hover:text-[#ffc554]'onClick={()=>setIsOpen(!isOpen)}>
+           <HiMenu />
+          </button>
+
+          <Link to="/reservations">Make A Reservation</Link>
+        </div>
+
+        {isOpen && (
+          <ul className='md:hidden flex flex-col gap-4 py-4'>
+            {links.map((link)=>(
+              <li key={link.path}>
+                <NavLink to={link.path} onClick={()=> setIsOpen(fales)} className={({isActive}) => `block transition hover:text-[#ffc554] ${isActive ? "text-[#748194]" :"text-[#ffc554]"}`}></NavLink>
+                {link.name}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </nav>
+  )
+}
+
+
+
+ {/* <li>
               <Link to="/">Home</Link>
             </li>
 
@@ -25,12 +69,4 @@ export default function NavBar() {
 
             <li>
               <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-
-          <Link to="/reservations">Make A Reservation</Link>
-        </div>
-      </div>
-    </nav>
-  )
-}
+            </li> */}
