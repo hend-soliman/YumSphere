1e1a08176c69b4b14ee409faf06b2fefe17c682c
karim-emad90 <karim.sharkawy90@gmail.com>
2026-01-16 15:26:32 +0200

    lastwork on main

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -3,17 +3,22 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import pizza from '../assets/best-foods/Blueberry-Kale-Smoothie.jpg'
 import qs from 'qs';
+import  API_URL from '../config/api';
 
-const STRAPI_URL = "http://localhost:1337";
+
+
+
 
 export default function MenuPage() {
   const [activeTab, setActiveTab] = useState("breakfast");
   const menuItems = ['breakfast','lunch','dinner','desserts','drinks'];
   const [menu,setMenu] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
   
 
 useEffect(() => {
-  const STRAPI_URL = "https://8d16dcd4d7d0.ngrok-free.app";
+  const STRAPI_URL = API_URL;
+  console.log(STRAPI_URL);
   
   // لاحظ استخدام النقاط (breakfast.img) لجلب الصورة من داخل المكون
   // تأكد من كتابة dessert و drink بدون S إذا كانت كذلك في Strapi
@@ -40,7 +45,7 @@ useEffect(() => {
     }}
   ></div>
   <div className="absolute inset-0 bg-black/35"></div>
-   <div className='relative z-30 w-[60%] flex justify-center items-center mb-[-24px] '>
+   <div className='relative z-30 w-full lg:w-[60%] flex justify-center items-center mb-[-24px] '>
           {menuItems.map((tab) => {
      
             return (
@@ -49,10 +54,10 @@ useEffect(() => {
             key={tab}
            onClick={() => setActiveTab(tab)}
 
-          className={`w-[20%] h-[40px] border border-gray-400 text-center flex justify-center items-center
+          className={`w-[20%] h-[40px] rounded-3xl text-sm lg:rounded-0 lg:text-lg border border-gray-400 text-center flex justify-center items-center
     transition-colors duration-200
     ${activeTab === tab
-      ? "bg-neutral-900 text-white font-semibold"
+      ? "bg-neutral-900 text-yellow-500 shadow-md scale-105 lg:scale-100 font-semibold border-0 border-b-3 border-yellow-500"
       : "bg-gray-100 text-gray-500 hover:bg-neutral-200 hover:text-black"}`}
         >{tab}</button>
           )})
@@ -78,7 +83,7 @@ useEffect(() => {
   {/* المحتوى */}
   <div className="relative z-20 mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 justify-items-center">
     {menu[activeTab]?.map((item) => {
-      const STRAPI_URL = "https://8d16dcd4d7d0.ngrok-free.app";
+      const STRAPI_URL = API_URL;
       const relativePath = item.img?.url;
       const fullImageUrl = relativePath ? `${STRAPI_URL}${relativePath}` : pizza;
 
@@ -100,9 +105,19 @@ useEffect(() => {
           <h4 className="text-xl h-[20%] text-black font-bold text-center">
             {item.name}
           </h4>
-          <div className="w-full h-[40%] rounded-2xl shadow border border-gray-300 overflow-y-auto bg-white/70">
-            <p className="p-2 w-full text-sm text-black">{item.desc}</p>
+          <div className="w-full h-[40%] rounded-2xl shadow border border-gray-300 overflow-y-auto bg-transparent">
+            <p className="p-2 w-full text-sm text-gray-600 
+          mt-1 
+          overflow-hidden
+          [display:-webkit-box]
+          [-webkit-line-clamp:4]
+          [-webkit-box-orient:vertical]">{item.desc}</p>
           </div>
+          <button
+          onClick={() => setSelectedItem(item)}
+          className=" self-end mr-1 text-xs text-amber-600 hover:underline"
+        >Read More</button>
+     
           <h2 className="text-black text-2xl self-center font-bold">
             EGP <span className="text-orange-600">{item.price}</span>
           </h2>
@@ -112,6 +127,30 @@ useEffect(() => {
         </div>
       );
     })}
+    {selectedItem && (
+  <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
+        onClick={() => setSelectedItem(null)}
+  >
+    <div className="bg-white max-w-md w-full rounded-xl p-6 relative"
+         onClick={(e) => e.stopPropagation()}
+         >
+      <button
+        onClick={() => setSelectedItem(null)}
+        className="absolute top-3 right-3 text-gray-400 hover:text-black"
+      >
+        ✕
+      </button>
+
+      <h3 className="text-xl font-semibold mb-3">
+        {selectedItem.name}
+      </h3>
+
+      <p className="text-gray-700 leading-relaxed whitespace-pre-line">
+        {selectedItem.desc}
+      </p>
+    </div>
+  </div>
+)}
 
     {/* حالة عدم وجود عناصر */}
     {(!menu[activeTab] || menu[activeTab].length === 0) && (
